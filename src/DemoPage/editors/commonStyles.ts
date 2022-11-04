56ad76a9c0daf5a27828a6780d0960f8cf9d9f28
unknown <edesbalazs@gmail.com>
2022-11-04 14:07:31 +0100

    removed v1 files

--- src/DemoPage/editors/commonStyles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/css'
-
-export const HeightSub = 130
-
-export const wrapperStyle = css`
-  padding: 16px;
-  height: calc(100vh - ${HeightSub}px);
-  overflow-y: auto;
-`
--- src/DemoPage/editors/configuration/overrideFields/OverrideEditorProps.tsx
+++
@@ -1,8 +0,0 @@
-import { PresetConfig, PresetConfigField } from '../../../../types'
-
-export type OverrideEditorProps = {
-  isDark: boolean
-  field: PresetConfigField
-  data: Partial<PresetConfig>
-  onChange: (data: Partial<PresetConfig>) => void
-}
